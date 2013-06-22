0f7df9d9b9404d542b204184d2f9c092d0d45b9a
Kristhyan <kristhyanl.zubia@gmail.com>
2013-06-22 17:22:50 -0500

    Mejore la funcion crear

--- include/player.cpp
+++ include/player.cpp
@@ -3,6 +3,7 @@
 Player::Player():id(sello), ini(false)
 {
     ++sello;
+    stLancha = stBuque = stPortaAviones = 1;
 }
 
 Player::~Player()
@@ -11,11 +12,24 @@ Player::~Player()
 }
 
 void Player::crear(int n){
+    if (n < 10)
+        n = 10;
+
+    for (;n > 10; n = n - 5){
+        ++stLancha; ++stBuque; ++stPortaAviones;
+    }
+
+    tam = n;
     ini = true;
     //name = name_;
     tabla = new int * [n];
     for (register int i = 0; i < n; ++i)
         tabla[i] = new int [n];
+
+    for(register int i = 0; i < tam; ++i){
+        for (register int j = 0; j < tam; ++j)
+            tabla[i][j] = 0;
+    }
 }
 
 int Player::sello = 0;
